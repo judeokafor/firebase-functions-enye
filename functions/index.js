eb4d774643396519cf75b6a55cb1d691ccc3165a
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-10-29 13:09:11 +0100

    fix uuid implementation

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,23 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const uuid = require('uuid/v5');
-
-const express = require('express');
-const cors = require('cors');
 
 admin.initializeApp();
-const app = express();
-app.use(cors());
-/**
- * post request to create a user
- * @param req
- * @param res
- */
-app.post('/', async (request, response) => {
-	try {
-		const user = request.body;
-		user.hobbies = user.hobbies.trim().split(/[ ,]+/);
-		user.id = uuid(`${user.firstname}${user.lastname}`, uuid.DNS);
-		await admin
-			.database()
-			.ref('/users')
-			.push(user);
-		return response.status(200).send(user);
-	} catch (error) {
-		return response.status(500).send('Oh no error:' + error);
-	}
-});
-app.get('/', async (request, response) => {
-	try {
-		return await admin
-			.database()
-			.ref('/users')
-			.on('value', snapshot => {
-				return response.status(200).send(snapshot.val());
-			});
-	} catch (error) {
-		return response.status(500).send('Oh no! Error: ' + error);
-	}
-});
-// app.delete('/:id', async (request, response) => {
-// 	try {
-// 		const { id } = request.params;
-// 		console.log('the id of the req', id);
-// 		// return response.send(id);
-// 		return await admin
-// 			.database()
-// 			.ref('/users/{id}')
-// 			.on('value', snapshot => {
-// 				return response.status(200).send(snapshot.val());
-// 			});
-// 	} catch (error) {
-// 		return response.status(500).send('Oh no! Error: ' + error);
-// 	}
-// });
 
-exports.entries = functions.https.onRequest(app);
+exports.addUser = functions.https.onRequest(async (req, res) => {
+	const user = req.body;
+	user.hobbies = user.hobbies.trim().split(/[ ,]+/);
+	const snapshot = await admin
+		.database()
+		.ref('/user')
+		.push({ user: user });
+	return res.status(200).json(snapshot.ref.toString());
+});
+exports.createUUID = functions.database
+	.ref('/user/{pushId}/user')
+	.onCreate((snapshot, context) => {
+		const user = snapshot.val();
+		const uuid = require('uuid/v5');
+		const useruuid = uuid(`${user.firstname}${user.lastname}`, uuid.DNS);
+		user.id = useruuid;
+		return snapshot.ref.parent.child('/').set(user);
+	});
